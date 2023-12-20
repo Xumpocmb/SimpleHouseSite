cded2cbc9cd23a257ea1ec914098c940a163fc8d
Xumpocmb <x.xumpocmb@gmail.com>
2023-12-20 11:49:44 +0300

    filters

--- home_app/urls.py
+++ home_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import index
 
+app_name = 'home_app'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('category/<int:category_id>/', index, name='category'),
 ]
--- home_app/views.py
+++ home_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 
-def index(request):
-    return render(request, 'home_app/index.html')
+def index(request, category_id=None):
+    context = {
+        'title': 'SimpleHouse',
+        'products': Product.objects.filter(category_id=category_id) if category_id else Product.objects.all(),
+        'categories': Category.objects.all()
+    }
+    return render(request, 'home_app/index.html', context)
