10cd6ec42c28bf57bc9fef1809f47e8c005aece6
Xumpocmb <x.xumpocmb@gmail.com>
2023-12-20 14:21:02 +0300

    email backend

--- SimpleHouse/settings.py
+++ SimpleHouse/settings.py
@@ -129,3 +129,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Sending emails
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = True
--- contacts_app/views.py
+++ contacts_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.template.loader import get_template
+from django.conf import settings
 from .models import ContactMessages
 from .forms import ContactMessagesForm
 from django.contrib import messages
+from django.core.mail import send_mail, EmailMultiAlternatives
 
 
 def contact_message(request):
@@ -15,8 +18,24 @@ def contact_message(request):
             email = request.POST.get('email')
             message = request.POST.get('message')
             ContactMessages.objects.create(name=name, email=email, message=message)
+            send_email(form.cleaned_data['name'], form.cleaned_data['email'], form.cleaned_data['message'])
             messages.success(request, 'Your message has been sent.')
     else:
         form = ContactMessagesForm()
         context['form'] = form
     return render(request, 'contacts_app/contact.html', context)
+
+
+def send_email(name, email, message):
+    text = get_template('contacts_app/message.html')
+    html = get_template('contacts_app/message.html')
+    context = {'name': name, 'email': email, 'message': message}
+    subject = 'Contact Form Message'
+    from_email = settings.EMAIL_HOST_USER
+    text_content = text.render(context)
+    html_content = text.render(context)
+
+    msg = EmailMultiAlternatives(subject, text_content, from_email, ['x.xumpocmb@gmail.com'])
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
